08edac07de5938fddbd8f9364f0b05f233409b54
JorgeGenovez <159270217+JorgeGenovez@users.noreply.github.com>
2024-10-31 19:52:05 -0300

    Create Dados.py

---
+++ Versão Teste 2/Dados.py
@@ -0,0 +1,10 @@
+class Variavel:
+    def __init__(self):
+        self.value = ""
+
+rua = Variavel()
+complemento = Variavel()
+bairro = Variavel()
+municipio = Variavel()
+estado = Variavel()
+cep = Variavel()
